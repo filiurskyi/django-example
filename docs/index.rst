6e2489eefc238e0605bd616b093199b134084b26
bubliq portable <anjey.mail@gmail.com>
2024-02-12 12:55:59 +0100

    add init doc html

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. django-example documentation master file, created by
+   sphinx-quickstart on Mon Feb 12 12:44:49 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to django-example's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Users app -> views/register
+=========================
+.. automodule:: users.views.register
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- users/views.py
+++ users/views.py
@@ -16,6 +16,16 @@ from .forms import (AddAuthorForm, AddQuoteForm, AddTagForm, LoginForm,
 
 
 def register(request):
+    """
+    The register function is responsible for handling the registration of new users.
+    It first checks if the user is already authenticated, and if so redirects them to the index page.
+    If not, it then checks whether or not a POST request has been made (i.e., whether or not a form has been submitted).
+    If so, it creates an instance of RegisterForm with that data and validates it; if valid, saves the form's data as a new User object in our database and redirects to login page; otherwise renders register template with invalid form instance passed as context. If no POST request was made (i.e.,
+    
+    :param request: Get the request object
+    :return: The rendered register
+    :doc-author: Trelent
+    """
     if request.user.is_authenticated:
         return redirect(to="quotes_app:index")
 
@@ -31,6 +41,16 @@ def register(request):
 
 
 def sign_in(request):
+    """
+    The sign_in function is a view that allows users to sign in.
+        If the user is already signed in, they are redirected to the index page.
+        If the request method is POST, then we authenticate and log them in if their username and password match.
+        Otherwise, we redirect them back to login with an error message.
+    
+    :param request: Pass the request object to the view
+    :return: A redirect to the index page if the user is already authenticated
+    :doc-author: Trelent
+    """
     if request.user.is_authenticated:
         return redirect(to="quotes_app:index")
     if request.method == "POST":
@@ -69,10 +89,28 @@ class DashboardView(ListView):
     # ordering = ["date_created"]
 
     def get_queryset(self):
+        """
+        The get_queryset function is used to filter the queryset of objects that are displayed in the view.
+        In this case, we're filtering it so that only objects created by the current user are shown.
+        
+        :param self: Represent the instance of the class
+        :return: A queryset of all the objects created by a user
+        :doc-author: Trelent
+        """
         queryset = super(DashboardView, self).get_queryset()
         return queryset.filter(created_by=self.request.user).order_by("date_created")
 
     def get_context_data(self, **kwargs):
+        """
+        The get_context_data function is a method that Django calls when rendering the template.
+        It allows you to add additional context variables to the template, which are then available in 
+        the rendered HTML. In this case, we're adding three variables: quotes, authors and tags.
+        
+        :param self: Represent the instance of the class
+        :param **kwargs: Pass keyworded, variable-length argument list
+        :return: A dictionary of data that is used to render the template
+        :doc-author: Trelent
+        """
         context = super(DashboardView, self).get_context_data(**kwargs)
         quotes = Quote.objects.filter(created_by=self.request.user).order_by(
             "-date_created"
